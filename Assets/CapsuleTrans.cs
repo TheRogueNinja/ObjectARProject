53fe8afad31d975c604d437703873f12e4a6b423
TheRogueNinja <shreyassawant018@gmail.com>
2017-06-27 14:12:27 +0530

    Object Tracking

---
+++ Assets/CapsuleTrans.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleTrans : MonoBehaviour {
+    public Transform caps;
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            caps.transform.Translate(new Vector3(0, 0, 2f));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            caps.transform.Translate(new Vector3(-2F, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            caps.transform.Translate(new Vector3(0, 0, -2f));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            caps.transform.Translate(new Vector3(2F, 0, 0));
+        }
+    }
+}
